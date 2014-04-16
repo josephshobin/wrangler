4370feac17c548a3db96eb9f2bea2cf90891e546
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-04-16 18:25:37 +1000

    Started adding travis support.

---
+++ src/main/scala/wrangler/api/Travis.scala
@@ -0,0 +1,21 @@
+package wrangler.api
+
+import java.io.File
+
+import scalaz._, Scalaz._
+
+/**
+  * Shells out to the travis client for ease of use.
+  * Expects the travis client to be logged in.
+  */
+object Travis {
+  def createBuild(repo: String): String \/ String =
+    Util.run(List("travis", "enable", "-r", repo))
+
+  def addSecureVariables(repoPath: String, variables: List[(String, String)]) =
+    variables.map { case (k, v) =>
+      Util.run(List("travis", "encrypt", s"$k=$v", "--add"), Some(new File(repoPath)))
+    }
+}
+
+
