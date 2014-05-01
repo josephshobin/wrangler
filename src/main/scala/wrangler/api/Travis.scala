4c5ef99a67d65f3b921e9c3cd64bd9e334482033
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-05-02 09:23:08 +1000

    Travis fix.

--- src/main/scala/wrangler/api/Travis.scala
+++ src/main/scala/wrangler/api/Travis.scala
@@ -12,10 +12,10 @@ object Travis {
   def createBuild(repo: String): String \/ String =
     Util.run(List("travis", "enable", "-r", repo))
 
-  def addSecureVariables(repoPath: String, variables: List[(String, String)]) =
+  def addSecureVariables(repoPath: String, variables: List[(String, String)]): String \/ List[String] =
     variables.map { case (k, v) =>
       Util.run(List("travis", "encrypt", s"$k=$v", "--add"), Some(new File(repoPath)))
-    }
+    }.sequenceU
 }
 
 
